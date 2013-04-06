ec0e31a1518fbb6901d26e25bba49e753297e73f
startling <tdixon51793@gmail.com>
2013-04-06 19:41:28 -0400

    replaced 'asRows' and 'asColumns' with monadic traversals 'rows' & 'columns'

--- Sheets.hs
+++ Sheets.hs
@@ -1,5 +1,6 @@
 module Sheets where
 -- base
+import Control.Applicative
 import Control.Monad
 import Data.List
 -- mtl
@@ -18,17 +19,17 @@ data Table m a = Table
   { fields :: [Column m a]
   , items  :: m [a]
   }
- 
+
 counter :: (Num n, Show s, MonadState s m) =>
   ASetter' s n -> Column m a
 counter l = Column . const . liftM (T.pack . show) $ increment l where
   increment :: (Num n, MonadState s m) => ASetter' s n -> m s
   increment l = liftM2 const get $ l += 1
 
-asColumns :: Monad m => Table m t -> m [[Text]]
-asColumns (Table c r) = mapM (($ r) . col) c where
-  col :: Monad m => Column m a -> m [a] -> m [Text]
-  col (Column a) r = r >>= mapM a
+rows :: Monad m => ([Text] -> m r) -> Table m t -> m [r]
+rows fn (Table cs ms) = ms >>= \is -> forM is $
+  \i -> forM cs (($ i) . field) >>= fn
 
-asRows :: Monad m => Table m t -> m [[Text]]
-asRows = liftM transpose . asColumns
+columns :: Monad m => ([Text] -> m c) -> Table m t -> m [c]
+columns fn (Table cs ms) = ms >>= \is -> forM cs $
+  \(Column c) -> forM is c >>= fn
