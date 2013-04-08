74f4d30486c3c15212569850013dcaf81b08385f
startling <tdixon51793@gmail.com>
2013-04-08 04:10:14 -0400

    split a table.

--- Sheets.hs
+++ Sheets.hs
@@ -27,6 +27,12 @@ rows fn (Table cs is) = forM is $ \i -> forM cs (($ i) . field) >>= fn
 columns :: Monad m => ([Text] -> m c) -> Table m t -> m [c]
 columns fn (Table cs is) = forM cs $ \(Field c) -> forM is c >>= fn
 
+-- | Split a table, given the number of rows each should have.
+split :: Int -> Table m a -> [Table m a]
+split n (Table fs is) = Table fs `map` taking n is where
+  taking :: Int -> [a] -> [[a]]
+  taking n x = let (a, b) = splitAt n x in a : taking n b
+  
 -- | A column taking a lens into a number in the state and
 -- counting up from that number.
 counter :: (Show n, Num n, MonadState s m) =>
@@ -37,3 +43,4 @@ counter l = Field . const $ (l += 1)
 -- | A column just showing the 'Text' in the row.
 see :: Monad m => Field m Text
 see = Field $ return . id
+
